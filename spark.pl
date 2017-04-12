516b4ad393b793a1db8756100a06ed9b12124af5
Cephas Armstrong-Mensah <carmstr7@users.noreply.github.com>
2017-04-11 23:51:39 -0700

    Update spark.pl
    Update from class notes from last Thursday

--- spark.pl
+++ spark.pl
@@ -1,40 +1,41 @@
-int([0], _).
-int --> \+ [0], int_.
-int_ --> digit, int_.
-int_ --> digit.
-digit --> [0].
-digit --> [1].
-digit --> [2].
-digit --> [3].
-digit --> [4].
-digit --> [5].
-digit --> [6].
-digit --> [7].
-digit --> [8].
-digit --> [9].
+% First block of lines represent the parser
+exp(ep(T,E)) --> term(T), ['+'], exp(E).
+exp(em(T,E)) --> term(T), ['*'], exp(E).
+exp(es(T,E)) --> term(T), ['-'], exp(E).
+exp(ed(T,E)) --> term(T), ['/'], exp(E).
+exp(er(T,E)) --> term(T), ['%'], exp(E).
+exp(et(T)) --> term(T).
 
-addition_symbol --> [+].
-subtraction_symbol --> [-].
-multiplication_symbol --> [*].
-division_symbol --> [/].
-% modulus_symbol --> ["%"].
+term(t(0)) --> [0].
+term(t(1)) --> [1].
+term(t(2)) --> [2].
+term(t(3)) --> [3].
+term(t(4)) --> [4].
+term(t(5)) --> [5].
+term(t(6)) --> [6].
+term(t(7)) --> [7].
+term(t(8)) --> [8].
+term(t(9)) --> [9].
 
-expression --> terminal, expression.
-expression --> terminal.
+% The next few lines are the evaluation functions
+ev_expr(ep(T, E),R) :- 	ev_term(T, R1),
+    				  	ev_expr(E, R2),
+    					R is R1 + R2.
+ev_expr(em(T, E),R) :- 	ev_term(T, R1),
+    				  	ev_expr(E, R2),
+    					R is R1 * R2.
+ev_expr(es(T, E),R) :- 	ev_term(T, R1),
+    				  	ev_expr(E, R2),
+    					R is R1 - R2.
+ev_expr(ed(T, E),R) :- 	ev_term(T, R1),
+    				  	ev_expr(E, R2),
+    					R is R1 / R2.
+ev_expr(er(T, E),R) :- 	ev_term(T, R1),
+    				  	ev_expr(E, R2),
+    					R is R1 mod R2.
+ev_expr(et(T),R)	:- 	ev_term(T, R).
+ev_term(t(T), T).
 
-terminal --> add.
-terminal --> subtract.
-terminal --> multiply.
-terminal --> divide.
-
-add --> int, addition_symbol, expression.
-add --> int.
-
-subtract --> int, subtraction_symbol, expression.
-subtract --> int.
-
-multiply --> int, multiplication_symbol, expression.
-multiply --> int.
-
-divide --> int, division_symbol, expression.
-divide --> int.
+% Query starts here eg. eval([7,'-',3],X)
+eval(Exp, Result) 	:- 	exp(T, Exp, []),
+    					          ev_expr(T, Result).
