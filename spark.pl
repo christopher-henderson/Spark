150e3e34803367c31047ea9c4fcc3734659cd8e8
David Henderson <therealdavidhenderson@gmail.com>
2017-04-13 13:39:50 -0700

    Formatting fixes and comments

--- spark.pl
+++ spark.pl
@@ -21,6 +21,7 @@ int(negative(Integer)) --> [-], int(Integer).
 int_(int(H, T)) --> digit(H), int_(T).
 int_(int(Digit)) --> digit(Digit).
 
+% Digit tokens
 digit(digit(0)) --> [0].
 digit(digit(1)) --> [1].
 digit(digit(2)) --> [2].
@@ -42,31 +43,32 @@ ev_expr(int(digit(D), Integer), R) :-
     R is D * X + R1.
 ev_expr(int(digit(D)), R) :- R is D.
 ev_expr(negative(Integer), R) :-
-  ev_expr(Integer, R1),
-  R is R1 * -1.
+    ev_expr(Integer, R1),
+    R is R1 * -1.
 
 % The next few lines are the evaluation functions
-ev_expr(ep(T, E),R) :-
+ev_expr(ep(T, E), R) :-
     ev_expr(T, R1),
     ev_expr(E, R2),
     R is R1 + R2.
-ev_expr(em(T, E),R) :-
+ev_expr(em(T, E), R) :-
     ev_expr(T, R1),
     ev_expr(E, R2),
     R is R1 * R2.
-ev_expr(es(T, E),R) :-
+ev_expr(es(T, E), R) :-
     ev_expr(T, R1),
     ev_expr(E, R2),
     R is R1 - R2.
-ev_expr(ed(T, E),R) :-
+ev_expr(ed(T, E), R) :-
     ev_expr(T, R1),
     ev_expr(E, R2),
     R is R1 / R2.
-ev_expr(er(T, E),R) :-
+ev_expr(er(T, E), R) :-
     ev_expr(T, R1),
     ev_expr(E, R2),
     R is R1 mod R2.
-ev_expr(et(T),R) :- 	ev_expr(T, R).
+ev_expr(et(T), R) :- 
+    ev_expr(T, R).
 ev_expr(t(T), T).
 
 % Query starts here eg. eval([7,'-',3],X)
