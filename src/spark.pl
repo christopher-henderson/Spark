b9bccdafb9253c11274f003cbc8adfdc810e14d6
Nelson Tran <nelson@nelsontran.com>
2017-04-23 22:54:04 -0700

    Implement C-style comments

--- src/spark.pl
+++ src/spark.pl
@@ -38,16 +38,37 @@ whitespace --> [' '].
 whitespace --> ['\t'].
 whitespace --> ['\n'].
 
-% The longest_match_alpha/3 predicate unifies with the longest string of
-% uppercase or lowercase letters.
+% Comment delimiters.
+comment_single --> ['/', '/'].
+comment_multi_start --> ['/', '*'].
+comment_multi_end --> ['*', '/'].
+
+% The longest_match_alpha/3 predicate unifies with the next longest string of
+% uppercase or lowercase letters in the list of remaining characters.
 longest_match_alpha([H|T]) --> [H], longest_match_alpha_(T), {char_type(H, alpha)}.
 longest_match_alpha_([H|T]) --> [H], longest_match_alpha_(T), {char_type(H, alpha)}.
 longest_match_alpha_([]) --> [].
 
-% The longest_match_int/3 predicate unifies with the longest string of digits.
+% The longest_match_int/3 predicate unifies with the next longest string of
+% digits in the list of remaining characters.
 longest_match_int([H|T]) --> [H], longest_match_int_(T), {char_type(H, digit)}.
 longest_match_int_([H|T]) --> [H], longest_match_int_(T), {char_type(H, digit)}.
 longest_match_int_([]) --> [].
+
+% The longest_match_no_nl/3 predicate unifies with the next longest string of
+% non-newline characters in the list of remaining characters.
+longest_match_no_nl([H|T]) --> [H], longest_match_no_nl_(T), {H \== '\n'}.
+longest_match_no_nl_([H|T]) --> [H], longest_match_no_nl_(T), {H \== '\n'}.
+longest_match_no_nl_([]) --> [].
+
+% The comment/3 predicate is used to consume a variable number of characters.
+% This is useful for consuming all characters between multi-line comment delimiters.
+comment --> [].
+comment --> [_], comment.
+
+% Comments.
+lexer(Tokens) --> comment_single, longest_match_no_nl(_), lexer(R), {Tokens = R}.
+lexer(Tokens) --> comment_multi_start, comment, comment_multi_end, lexer(R), {Tokens = R}.
 
 % Identifiers and integer literals.
 lexer(Tokens) --> longest_match_alpha(I), lexer(R), {atom_chars(IA, I), append([IA], R, Tokens)}.
